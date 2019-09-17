477bfd96b9084c3572fee0ef8769996c5f801ede
Shaina Tran <ShainaATran@gmail.com>
2019-09-17 01:10:14 -0700

    DNAManager made

---
+++ DNA Game/Assets/DNAManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DNAManager : MonoBehaviour
+{
+
+    public List<Nucleotide> nucleotides;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CreateDNA();
+    }
+
+    public void CreateDNA()
+    {
+        var chars = "ATCG";
+        var stringChars = new char[8];
+        var random = new System.Random();
+
+        for (int i = 0; i < stringChars.Length; i++)
+        {
+            stringChars[i] = chars[random.Next(chars.Length)];
+            Nucleotide nucleotide = new Nucleotide(stringChars[i]);
+            Debug.Log(nucleotide.nucleotide);
+            nucleotides.Add(nucleotide);
+        }
+
+        var finalString = new String(stringChars);
+        Console.WriteLine(finalString);
+
+    }
+
+}
---
+++ DNA Game/Assets/Nucleotide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Nucleotide
+{
+    public char nucleotide;
+
+    public Nucleotide(char n)
+    {
+        nucleotide = n;
+    }
+}
